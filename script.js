df8f4d1859789e95305a15a987628d02e3e373ff
rayoghossan <rayoghossan200@gmail.com>
2024-02-21 13:46:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+alert ("aplikasi Tiket");
+
+const nama = prompt("masukan nama");
+const umur = prompt("masukan umur");
+
+
+
+if (umur <=13 ) {
+    alert (`anda masih belum cukup umur`);
+}
+
+const studio = prompt("pilih studio A,B,C(Kapital)");
+
+if (studio == "A" ) {
+    alert (`Tiket ${nama}, studio A umur ${umur}`);
+} else if (nilai >= "B") {
+    alert (`Tiket ${nama}, studio B umur ${umur}`);
+} else if (nilai >= "C") {
+    alert (`Tiket ${nama}, studio C umur ${umur}`);
+}
